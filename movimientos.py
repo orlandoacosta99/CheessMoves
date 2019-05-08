96c8d71c4eee9ec6b8f9bbcfc672110e4ad18cb5
Orlando Camilo Acosta <oacosta00309@universidadean.edu.co>
2019-05-07 21:18:59 -0500

    modificacion ajtorre

--- movimientos.py
+++ movimientos.py
@@ -37,3 +37,44 @@ def obtener_nombre_pieza(simbolo):
         return 'Rey ' + tipo
     else:
         return 'No es una pieza'
+
+
+def mover_torre(tablero, x_inicial, y_inicial, x_final, y_final):
+    """
+    >>> mover_torre(tablero[[]],0,0,1,0)
+    :param tablero:
+    :param x_inicial:
+    :param y_inicial:
+    :param x_final:
+    :param y_final:
+    :return:
+    """
+
+    # tablero = [
+    #     ['t', 'k', 'a', 'q', 'r', 'a', 'k', 't'],
+    #     ['p', 'p', 'p', 'p', 'p', 'p', 'p', 'p'],
+    #     [' ', ' ', ' ', ' ', ' ', ' ', ' ', ' '],
+    #     [' ', ' ', ' ', ' ', ' ', ' ', ' ', ' '],
+    #     [' ', ' ', ' ', ' ', ' ', ' ', ' ', ' '],
+    #     [' ', ' ', ' ', ' ', ' ', ' ', ' ', ' '],
+    #     ['P', 'P', 'P', 'P', 'P', 'P', 'P', 'P'],
+    #     ['T', 'K', 'A', 'R', 'Q', 'A', 'K', 'T']
+    # ]
+
+    tablerocop = tablero.copy()
+    if (x_inicial == x_final or y_inicial == y_final) and tablerocop[x_inicial][y_inicial].lower() == 't':
+        if x_inicial != x_final:
+            for x in range(x_inicial + 1, x_final):
+                if tablerocop[x][y_inicial] != ' ':
+                    raise ValueError('El camino no es valido')
+        tablerocop[x_final][y_inicial] = 't'
+        tablerocop[x_inicial][y_inicial] = ' '
+        if y_inicial != y_final:
+            for y in range(y_inicial + 1, y_final):
+                if tablerocop[x_inicial][y] != ' ':
+                    raise ValueError('El camino no es valido')
+        tablerocop[x_inicial][y_final] = 't'
+        tablerocop[x_inicial][y_inicial] = ' '
+    return tablerocop
+
+
